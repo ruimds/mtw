dbd9235aa5f201adbd6d38e6bdacb0b68df0cda6
Bruno Silva <55957337+brunofpsilva@users.noreply.github.com>
2020-06-05 19:18:03 +0100

    Registo, Login , Routes

---
+++ ProjetoMultimedia/app/src/app/app-routing-module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from "@angular/core";
+import { Routes } from "@angular/router";
+import { RouterModule } from "@angular/router";
+import { LoginPacienteComponent } from "./components/auth/login-paciente/login-paciente/login-paciente.component";
+import { LoginPsicologoComponent } from "./components/auth/login-psicologo/login-psicologo/login-psicologo.component";
+import { HomeComponent } from "./components/Home/home.component";
+import { HomePacienteComponent } from "./components/Home-Paciente/home-paciente.component";
+import { HomePsicologoComponent } from "./components/Home-Psicologo/home-psicologo.component";
+
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "paciente", component: HomePacienteComponent },
+  { path: "psicologo", component: HomePsicologoComponent },
+  { path: "loginPaciente", component: LoginPacienteComponent },
+  { path: "loginPsicologo", component: LoginPsicologoComponent },
+  { path: "registoPaciente", component: LoginPsicologoComponent },
+  { path: "registoPsicologo", component: LoginPsicologoComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ ProjetoMultimedia/app/src/app/components/auth/Registo-Paciente/registo-paciente.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit, NgModule, Input } from "@angular/core";
+
+@Component({
+  selector: "app-registo-paciente",
+  templateUrl: "./registo-paciente.component.html",
+  styleUrls: ["./registo-paciente.component.css"],
+})
+export class RegistoPacienteComponent {}
