df2f509d60c6bd815d4b87cde90f3e0ab64fb69d
Rui Santos <ruidsantos99@gmail.com>
2020-06-06 21:18:47 +0100

    Add files for srs form component

---
+++ ProjetoMultimedia/app/src/app/components/srs/srs-component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit, NgModule, Input } from "@angular/core";
+
+@Component({
+  selector: "app-srs",
+  templateUrl: "./srs-component.html",
+  styleUrls: ["./srs-component.css"],
+})
+export class srsComponent {}
