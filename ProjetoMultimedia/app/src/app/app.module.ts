ba85b37c0921d6b199af2921a8016683fa44b888
Rui Santos <ruidsantos99@gmail.com>
2020-06-06 18:13:55 +0100

    Add hammerjs for gestures

--- ProjetoMultimedia/app/src/app/app.module.ts
+++ ProjetoMultimedia/app/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
+/**
+ * Components
+ */
 import { AppComponent } from "./app.component";
 import { LoginPacienteComponent } from "./components/auth/login-paciente/login-paciente/login-paciente.component";
 import { LoginPsicologoComponent } from "./components/auth/login-psicologo/login-psicologo/login-psicologo.component";
@@ -9,14 +12,22 @@ import { RegistoPsicologoComponent } from "./components/auth/Registo-Psicologo/r
 import { HomeComponent } from "./components/Home/home.component";
 import { HomePsicologoComponent } from "./components/Home-Psicologo/home-psicologo.component";
 import { HomePacienteComponent } from "./components/Home-Paciente/home-paciente.component";
+import { orsComponent } from "./components/ORS/ors.component";
+
+ /**
+  * Modules
+  */
+import {MatSliderModule} from '@angular/material/slider';
 import { HeaderComponent } from "./components/Header/header.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { AppRoutingModule } from "./app-routing-module";
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatCardModule} from '@angular/material/card';
 
-import { orsComponent } from "./components/ORS/ors.component";
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -37,6 +48,9 @@ import { orsComponent } from "./components/ORS/ors.component";
     MatIconModule,
     MatToolbarModule,
     AppRoutingModule,
+    MatSliderModule,
+    MatCheckboxModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent],
